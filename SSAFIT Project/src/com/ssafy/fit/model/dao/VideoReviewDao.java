b37657af1f7c16300be5dd2ebf921c75c32eb69c
damdam6 <dambi626626@gmail.com>
2023-08-04 13:22:37 +0900

    move

--- SSAFIT Project/src/com/ssafy/fit/model/dao/VideoReviewDao.java
+++ SSAFIT Project/src/com/ssafy/fit/model/dao/VideoReviewDao.java
@@ -1,5 +1,11 @@
 package com.ssafy.fit.model.dao;
 
+import java.util.List;
+
+import com.ssafy.fit.model.VideoReview;
+
 public interface VideoReviewDao {
 
+	boolean insertReview(VideoReview videoReview);
+	List<VideoReview> selectReview(int videoNo);
 }
