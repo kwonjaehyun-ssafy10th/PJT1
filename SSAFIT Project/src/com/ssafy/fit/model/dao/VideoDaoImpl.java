d05be6c459453b5714c4d6ec6fcf0f6f1debf095
kwonjaehyun-ssafy10th <kwonjaehyun.ssafy@gmail.com>
2023-08-04 12:23:20 +0900

    Video,VideoDao, VideoDaoImpl first commit

--- SSAFIT Project/src/com/ssafy/fit/model/dao/VideoDaoImpl.java
+++ SSAFIT Project/src/com/ssafy/fit/model/dao/VideoDaoImpl.java
@@ -1,5 +1,26 @@
 package com.ssafy.fit.model.dao;
 
-public class VideoDaoImpl implements VideoDao {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.ssafy.fit.model.Video;
+
+public class VideoDaoImpl implements VideoDao {
+	private List<Video> list = new ArrayList<>();
+	static private VideoDaoImpl instance = new VideoDaoImpl();
+	
+	private VideoDaoImpl() {}
+	static public VideoDaoImpl getInstance() {
+		return instance;
+	}
+	
+	public List<Video> selectVideo() {
+		return list;	
+	}
+	
+	public Video selectVideoByNo(int no) {
+		return null;
+		
+	}
+	
 }
